65457602c551b5fc7e783694c289b117c962b6ad
Makanaka-Mitchelle <chawiramitchelle@gmail.com>
2023-10-04 12:24:36 -0700

    Config

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - Entry point of the program
@@ -9,6 +10,7 @@
 int main(int argc, char *argv[])
 {
 	(void) argc;
+
 	printf("%s\n", argv[0]); 
 	return (0);
 }
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,6 +13,7 @@ int main(int argc, char *argv[])
 	int num1;
 	int num2;
 	int result;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
@@ -20,7 +21,7 @@ int main(int argc, char *argv[])
 	}
 
 	num1 = atoi(argv[1]);
-       	num2 = atoi(argv[2]);
+	num2 = atoi(argv[2]);
 	result = num1 * num2;
 
 	printf("%d\n", result);
